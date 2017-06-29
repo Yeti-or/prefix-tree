6130e9b670d3a289e5449727f04d917b3224358a
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-06-30 02:34:02 +0300

    Add tests

--- test/index.js
+++ test/index.js
@@ -5,3 +5,58 @@ var Tree = require('../lib');
 it('should provide a Function', () => {
     expect(Tree).to.be.a('Function');
 });
+
+describe('set', () => {
+    it('should return itself', () => {
+        var tree = new Tree();
+        expect(tree.set('hello', 'world')).to.be.instanceof(Tree);
+    });
+});
+
+describe('get', () => {
+    it('should return an Array', () => {
+        var tree = new Tree();
+        tree.set('hello', 'world');
+        expect(tree.get('hello')).to.be.an('Array');
+    });
+
+    it('should accept no args', () => {
+        var tree = new Tree();
+        tree.set('hello', 'world');
+        expect(tree.get()).to.be.an('Array');
+    });
+
+    it('should add key/value', () => {
+        var tree = new Tree();
+        tree.set('hello', 'world');
+        expect(tree.get('hello')).to.eql(['world']);
+    });
+
+    it('should return several values for prefix', () => {
+        var tree = new Tree();
+        tree.set('hell', 666);
+        tree.set('hello', 42);
+        expect(tree.get('he')).to.eql([666, 42]);
+    });
+
+    it('should not return values of different prefix', () => {
+        var tree = new Tree();
+        tree.set('hell', 666);
+        tree.set('hello', 42);
+        expect(tree.get('xo')).to.have.lengthOf(0);
+    });
+});
+
+describe('constructor', () => {
+    it('should create tree with several items', () => {
+        var tree = new Tree([['hello', 'hello'], ['hi', 'hi'], ['ho', 'ho']])
+        expect(tree.get('h')).to.have.lengthOf(3);
+    });
+});
+
+describe('toString', () => {
+    it('should return String', () => {
+        var tree = new Tree([['hello', 'hello'], ['hi', 'hi'], ['ho', 'ho']])
+        expect(tree.toString()).to.be.a('String');
+    });
+});
